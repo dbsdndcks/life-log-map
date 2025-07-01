853b0af6a9b2ba7f20f24c928e8e4e3dd8e89878
DEV_WOONG <ywcsuwon@gmail.com>
2025-07-01 18:13:35 +0900

    feat: setup project

---
+++ src/main/java/com/lifelogmap/server/LifelogServerApplication.java
@@ -0,0 +1,13 @@
+package com.lifelogmap.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifelogServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifelogmap/server/LifelogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifelogmap.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
